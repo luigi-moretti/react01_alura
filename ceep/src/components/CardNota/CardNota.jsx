1e7c9f2d6c9957bf0ad48fab0d3ce40983d7d0b6
Luigi Moretti <luigi_morettii@hotmail.com>
2020-08-19 09:21:45 -0300

    Importando SVGs no React

--- ceep/src/components/CardNota/CardNota.jsx
+++ ceep/src/components/CardNota/CardNota.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg"
+//import  deleteSVG from "../../assets/img/delete.svg" importando como imagem
 class CardNota extends Component {
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+        { /* <img src={deleteSVG} /> */}
+          <DeleteSVG/>
         </header>
     <p className="card-nota_texto">{this.props.texto}</p>
       </section>
